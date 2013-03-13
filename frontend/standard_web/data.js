f9acab4c0f1ea017b462afbeeba48900583d7151
Hongxia <hongxia.zhong@gmail.com>
2013-03-13 11:57:48 -0700

    allow applyLocationData function to ignore hour and minute info in ts object

--- frontend/standard_web/data.js
+++ frontend/standard_web/data.js
@@ -13,16 +13,29 @@ var CALLBACK_URL = "&callback=?";
 //		);
 // Warning: Notice that the "timestamp" in the string output is not wrapped with quotation marks
 // i.e. 2013-03-14 15:00:00 instead of "2013-03-14 15:00:00"
-var applyLocationData = function(callback_func, lat, lon, rad, ts) {
+var applyLocationData = function(callback_func, lat, lon, rad, ts, discard_hour, discard_minute) {
 	var coord_url = "";
 	var rad_url = "";
 	if ((typeof lat !== 'undefined') && (typeof lon !== 'undefined') && (typeof rad !== 'undefined')) {
 		coord_url = "coord/"+lat+"/"+lon+"/";
 		rad_url = "rad/"+rad+"/";
 	}
+
+	if (typeof discard_hour === 'undefined') {
+		discard_hour = false;
+	}
+	if (typeof discard_minute === 'undefined') {
+		discard_minute = false;
+	}
 	var ts_url = "";
 	if (typeof ts !== 'undefined') {
-		ts_url = "ts/"+ts.getYear()+"/"+ts.getMonth()+"/"+ts.getDate()+"/"+ts.getHours()+"/"+ts.getMinutes()+"/";
+		ts_url = "ts/"+ts.getYear()+"/"+ts.getMonth()+"/"+ts.getDate()+"/";
+		if (!discard_hour) {
+			ts_url += ts.getHours()+"/";
+			if (!discard_minute) {
+				ts_url += ts.getMinutes()+"/";
+			}
+		}
 	}
 	$.getJSON(SERVER_URL+HEATMAP_SEARCH_URL+coord_url+rad_url+ts_url+CALLBACK_URL,
 		function(response) {
@@ -36,16 +49,29 @@ var applyLocationData = function(callback_func, lat, lon, rad, ts) {
     );
 }
 
-var applyLocationDataReg = function(callback_func, lat, lon, latrange, lonrange, ts) {
+var applyLocationDataReg = function(callback_func, lat, lon, latrange, lonrange, ts, discard_hour, discard_minute) {
 	var coord_url = "";
 	var reg_url = "";
 	if ((typeof lat !== 'undefined') && (typeof lon !== 'undefined') && (typeof latrange !== 'undefined') && (typeof lonrange !== 'undefined')) {
 		coord_url = "coord/"+lat+"/"+lon+"/";
 		reg_url = "reg/"+latrange+"/"+lonrange+"/";
 	}
+
+	if (typeof discard_hour === 'undefined') {
+		discard_hour = false;
+	}
+	if (typeof discard_minute === 'undefined') {
+		discard_minute = false;
+	}
 	var ts_url = "";
 	if (typeof ts !== 'undefined') {
-		ts_url = "ts/"+ts.getYear()+"/"+ts.getMonth()+"/"+ts.getDate()+"/"+ts.getHours()+"/"+ts.getMinutes()+"/";
+		ts_url = "ts/"+ts.getYear()+"/"+ts.getMonth()+"/"+ts.getDate()+"/";
+		if (!discard_hour) {
+			ts_url += ts.getHours()+"/";
+			if (!discard_minute) {
+				ts_url += ts.getMinutes()+"/";
+			}
+		}
 	}
 	$.getJSON(SERVER_URL+HEATMAP_SEARCH_URL+coord_url+rad_url+ts_url+CALLBACK_URL,
 		function(response) {
