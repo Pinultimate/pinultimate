096e0d413e6f99ea5688da446b004a29929eaf1a
Hongxia <hongxia.zhong@gmail.com>
2013-03-12 17:41:26 -0700

    modified server/settings.py so that it does not require mysql connection as default db backend.

--- server/server/settings.py
+++ server/server/settings.py
@@ -17,15 +17,7 @@ connect(DBNAME)
 
 # This is IGNORED when using MongoDB
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'HEATMAPDB',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'pudevs',
-        'PASSWORD': 'pukey',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
+    'default': {}
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
