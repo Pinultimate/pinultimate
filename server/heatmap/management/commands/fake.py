86e4d0fcbba7ba4fc23722aaaf6676d96d7d5ec9
Hongxia <hongxia.zhong@gmail.com>
2013-03-13 12:21:53 -0700

    make fake-data-generator more robust, allow specifying timespan

--- server/heatmap/management/commands/fake.py
+++ server/heatmap/management/commands/fake.py
@@ -75,6 +75,16 @@ class Command(BaseCommand):
                         default=DBNAME,
                         help='db to store points in'
             ),
+            make_option('--start_time',
+                        type=str,
+                        default='2000-1-1 0:0:0',
+                        help='beginning time of the generated fake data'
+            ),
+            make_option('--num_hours',
+                        type=int,
+                        default=1,
+                        help='number of hours to span'
+            ),
             make_option('-p',
                         '--print',
                         dest='output_method',
@@ -92,8 +102,8 @@ class Command(BaseCommand):
 
         gen = LocationGenerator(options['db_name'], self)
         center = {'latitude':options['lat'], 'longitude':options['lon']}
-        t = datetime.datetime.utcnow()
+        t = datetime.datetime.strptime(options['start_time'],'%Y-%m-%d %H:%M:%S')
 
-        for x in range(3):
-            gen.generate_locations(center, options['latrange'], options['lonrange'], t - datetime.timedelta(hours=x), options['num_per_hour'])
+        for x in range(options['num_hours']):
+            gen.generate_locations(center, options['latrange'], options['lonrange'], t + datetime.timedelta(hours=x), options['num_per_hour'])
         options['output_method'](gen)
