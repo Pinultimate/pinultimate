209b2586a5d0c893e2ac8ec84e1035e25618407a
eyuelt <eyueltessema@gmail.com>
2013-04-16 17:00:12 -0700

    get flickr scraper working with django

---
+++ server/flickr/management/commands/flickr.py
@@ -0,0 +1,44 @@
+from django.core.management.base import BaseCommand
+import json, urllib2
+from flickr.models import flickrLocation
+from server.settings import DBNAME
+import mongoengine as mongo
+import datetime
+
+API_KEY = 'c713991eeaef515a3fdacafdc0a140c4'
+LAT = 37.4419
+LON = -122.1419
+QUERYSTRING_FORMAT = 'http://api.flickr.com/services/rest/?method=flickr.photos.search&per_page=250&license=0,1,2,3,4,5,6,7&has_geo=1&extras=original_format,tags,description,geo,date_taken,date_upload,owner_name,place_url&format=json&nojsoncallback=1&api_key={0}&lat={1}&lon={2}&min_upload_date={3}&page={4}'
+
+def write_to_db(json_data):
+    #make sure to count all pics by the same user in one hour as one 'checkin'
+    for elem in json_data:
+        date_taken = datetime.datetime(int(time.mktime(time.strptime(elem['datetaken'], "%Y-%m-%d %H:%M:%S"))))
+        location = FlickrLocation(
+            coordinates = [elem['latitude'], elem['longitude']],
+            timestamp = date_taken,
+            user_id = elem['ownername'],
+        )
+        location.save()
+
+def get_min_upload_date():
+    return 0;
+    
+def main():
+    page = 1
+    querystring = QUERYSTRING_FORMAT.format(API_KEY, LAT, LON, get_min_upload_date(), page)
+        
+    while page < 1000:
+        print 'getting page #{0}'.format(page)
+        data = json.loads(str(urllib2.urlopen(querystring).read()))
+        print int(data['photos']['pages'])
+        write_to_db(data['photos']['photo']);
+        if page >= int(data['photos']['pages']):
+            break;
+        page += 1
+        querystring = QUERYSTRING_FORMAT.format(API_KEY, LAT, LON, get_min_upload_date(), page)
+            
+
+if __name__ == '__main__':
+    mongo.connect(DBNAME)
+    main()
---
+++ server/flickr/models.py
@@ -0,0 +1,16 @@
+from mongoengine import DynamicDocument, GeoPointField, DateTimeField, StringField
+
+class Location(DynamicDocument):
+    coordinates = GeoPointField(required=True)
+    timestamp = DateTimeField(required=True)
+    
+    def __unicode__(self):
+        return 'Coordinates: %s' % self.coordinates
+    
+    meta = {
+        'ordering': ['timestamp'],
+    }
+    
+class FlickrLocation(Location):
+    source = StringField(default="flickr")
+    user_id = StringField()
