08f676e85c4007efbd224ff67dd548431b074d69
eyuelt <eyueltessema@gmail.com>
2013-05-11 02:42:04 -0700

    Modify instagram script to reflect new subdomain for api calls

--- scripts/instagram_subscribe.bash
+++ scripts/instagram_subscribe.bash
@@ -8,5 +8,5 @@ curl -F 'client_id=f6667e2778b84c5499a5fe4f8e207757' \
 	-F 'lng=-122.1419' \
 	-F 'radius=5000' \
 	-F 'verify_token=geography' \
-	-F 'callback_url=http://pinultimate.net/instagram/subscription/' \
+	-F 'callback_url=http://api.pinultimate.net/instagram/subscription/' \
 	https://api.instagram.com/v1/subscriptions/
