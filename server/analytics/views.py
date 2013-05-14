315fa47efbb76f985722c518402be0ec1099601b
Firas Abuzaid <fabuzaid21@gmail.com>
2013-05-14 16:39:21 -0700

    fixed CSRF problem that prevented POST requests

--- server/analytics/views.py
+++ server/analytics/views.py
@@ -1,13 +1,21 @@
 from webapp.models import WebUser
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
+@require_POST
 def tap(request):
-	if request.method == 'POST':
-		web_user = WebUser.objects.get(session=request.session)
-		if web_user is not None:
-			web_user.tap()
+    web_user = WebUser.objects.get(session=request.session)
+    if web_user is not None:
+        web_user.tap()
+    return HttpResponse(status=200)
 
+
+@csrf_exempt
+@require_POST
 def update(request):
-	if request.method == 'POST':
-		web_user = WebUser.objects.get(session=request.session)
-		if web_user is not None:
-			web_user.update()
+    web_user = WebUser.objects.get(session=request.session)
+    if web_user is not None:
+        web_user.update()
+    return HttpResponse(status=200)
