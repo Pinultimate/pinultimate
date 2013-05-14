72685dc197ca8ee700b35b2aef9e658994c4ba2e
Hongxia <hongxia.zhong@gmail.com>
2013-05-14 00:14:18 -0700

    implemented analytics functions in the models

--- server/analytics/models.py
+++ server/analytics/models.py
@@ -8,8 +8,25 @@ class User(DynamicDocument):
     def __unicode__(self):
         return self.email
 
-#    def get_session_count(self):
- #       return self.
+    def get_all_sessions(self):
+        return UsageSession.objects(user=self)
+
+    def get_session_count(self):
+        return len(self.get_all_sessions())
+
+    def get_total_active_seconds(self):
+        active_seconds = 0
+        sessions = self.get_all_sessions()
+        for session in sessions:
+            active_seconds = active_seconds + session.get_active_seconds()
+        return active_seconds
+
+    def get_total_cluster_taps(self):
+        taps = 0
+        sessions = self.get_all_sessions()
+        for session in sessions:
+            taps = taps + session.cluster_taps
+        return taps
 
 class UsageSession(DynamicDocument):
     user = ReferenceField(User)
@@ -19,6 +36,10 @@ class UsageSession(DynamicDocument):
 
     def __unicode__(self):
         return '%s: %s - %s' % (user, login, logout)    
+
+    def get_active_seconds(self):
+        span = self.logout - self.login
+        return span.seconds
 
     meta = {
         'ordering': ['user']
