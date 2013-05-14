0da9ee10f9cb179c45162bcdd664d61cbd131b96
Firas Abuzaid <fabuzaid21@gmail.com>
2013-05-14 04:09:43 -0700

    switched from login-driven analytics to session-drive analytics

--- server/analytics/views.py
+++ server/analytics/views.py
@@ -1,16 +1,13 @@
 from webapp.models import WebUser
-from django.contrib.auth.decorators import login_required
 
-@login_required()
 def tap(request):
 	if request.method == 'POST':
-		web_user = WebUser.objects.get(user=request.user)
+		web_user = WebUser.objects.get(session=request.session)
 		if web_user is not None:
 			web_user.tap()
 
-@login_required()
 def update(request):
 	if request.method == 'POST':
-		web_user = WebUser.objects.get(user=request.user)
+		web_user = WebUser.objects.get(session=request.session)
 		if web_user is not None:
 			web_user.update()
--- server/webapp/models.py
+++ server/webapp/models.py
@@ -1,23 +1,23 @@
 from django.contrib.auth import models
-from models import User
+from django.contrib.sessions.models import Session
 from datetime import datetime
 
-class WebUser(models.Model):
-    user = models.ForeignKey(User)
+class WebUser(Session):
+    session = models.ForeignKey(Session)
     total_taps = models.IntegerField(default=0)
     total_seconds_spent = models.IntegerField(default=0)
-    total_logins = models.IntegerField(default=0)
+    total_visits = models.IntegerField(default=0)
     last_updated = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
-    	return user
+        return self.session
 
-	def update(self):
-		now = datetime.now()
-		lapse = now - self.last_updated
-		self.total_seconds_spent += lapse.seconds
-		self.last_updated = now
-		self.save()
+    def update(self):
+        now = datetime.now()
+        lapse = now - self.last_updated
+        self.total_seconds_spent += lapse.seconds
+        self.last_updated = now
+        self.save()
 
     def tap(self):
         self.total_taps += 1
